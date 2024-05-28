ae9e56d92b34919816af3a26b70a3266225c9e0c
Kamrul Hasan <63355519+khriad991@users.noreply.github.com>
2024-05-28 12:50:41 +0600

    completed page of Refund & terms

--- src/app/legals/terms/page.js
+++ src/app/legals/terms/page.js
@@ -1,14 +1,14 @@
 import React, {Suspense} from 'react';
 import MasterLayout from "@/components/master/MasterLayout";
 import LegalSkeleton from "@/skeleton/legal-skeleton";
-import Refund from "@/components/legals/Refund";
+import Terms from "@/components/legals/Terms";
 
 const Page = () => {
     return (
         <MasterLayout>
-           <Suspense fallback={<LegalSkeleton/>}>
-               <Refund/>
-           </Suspense>
+            <Suspense fallback={<LegalSkeleton/>}>
+                <Terms/>
+            </Suspense>
         </MasterLayout>
     );
 };
